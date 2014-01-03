842f7f7263fd98a1dcfc482b37b79bc3057dceb7
saerem21 <nemessisdds>
2014-01-03 18:12:42 +0900

    107 PAGE 실습
    3개의 텍스트 필드생성
    각 텍스트는 태그로 구분
    첫번째 는 리턴누르면 키보드사라짐
    두번째는 편집불가
    세번째는 z 입력불가
    세번째는 코드로 작

--- TextFieldTest/ViewController.m
+++ TextFieldTest/ViewController.m
@@ -15,12 +15,44 @@
 
 @implementation ViewController
 
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField{
+    if(2 == textField.tag)return NO;
+    return YES;
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSLog(@"input : %@",textField.text);
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    if(3 == textField.tag && NSOrderedSame == [string compare:@"z" options:NSCaseInsensitiveSearch])return NO;
+    //z가 입력된경우 는 return NO
+    return YES;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self.textInput becomeFirstResponder];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 205, 0, 0)];
+    label.text = @"z입력금지";
+    label.textColor = [UIColor whiteColor];
+    label.backgroundColor = [UIColor clearColor];
+    [label sizeToFit];
+    [self.view addSubview:label];
+    
+    UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(130, 200, 171, 31)];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.delegate = self;
+    textField.tag = 3;
+    
+    [self.view addSubview:textField];
 }
+
 - (IBAction)keyboardOff:(id)sender {
      [self.textInput resignFirstResponder];
 }
